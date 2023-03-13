e362a78e439f536537392deed05e510c9f99d4ca
Jono <jono@foodnotblogs.com>
2023-03-13 11:47:50 -0700

    Updates

--- hasura/migrations/default/1674050851668_event_label/down.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE eventd DROP COLUMN label;
--- hasura/seeds/default/1674002734661_nbLocations.sql
+++
@@ -1,4 +0,0 @@
-SET check_function_bodies = false;
-INSERT INTO public.locations (id, uuid, name) VALUES (1, 'dd982b3d-70d2-4f47-912b-2ea3a5ef8115', 'North Berkeley BART -> SF Financial District');
-INSERT INTO public.locations (id, uuid, name) VALUES (2, 'fe4b4a8a-bbcc-456a-a072-1f679e116b6e', 'North Berkeley BART -> SF Civic Center');
-SELECT pg_catalog.setval('public.locations_id_seq', 4, true);
