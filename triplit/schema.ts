a64239577bd1b2188cb2204df1be967973373fa9
Jono <jono@foodnotblogs.com>
2025-10-26 10:17:34 -0700

    npm fixes

--- triplit/schema.ts
+++ triplit/schema.ts
@@ -1,32 +1,10 @@
 import { Schema as S } from "@triplit/client";
 
-// Using different ID formats
 export const schema = S.Collections({
-  // todos: {
-  //   schema: S.Schema({
-  //     id: S.Id({ format: "nanoid" }), // default
-  //     text: S.String({}),
-  //     // ... other fields
-  //   }),
-  // },
-  // users: {
-  //   schema: S.Schema({
-  //     id: S.Id({ format: "uuidv4" }),
-  //     // ... other fields
-  //   }),
-  // },
-  // posts: {
-  //   schema: S.Schema({
-  //     id: S.Id({ format: "uuidv7" }),
-  //     // ... other fields
-  //   }),
-  // },
   locations: {
     schema: S.Schema({
-      id: S.Id(),
-      //   id: S.Id({ format: "uuidv4" }),
+      id: S.Id(), // nanoid
       name: S.String({}),
-      // ... other fields
 
       //   CREATE TABLE locations (
       //     id serial4 NOT NULL,
@@ -35,6 +13,13 @@ export const schema = S.Collections({
       //     constraint locations_pkey primary key (id)
       // );
     }),
+    permissions: {
+      anonymous: {
+        read: {
+          filter: [true],
+        },
+      },
+    },
   },
   events: {
     schema: S.Schema({
@@ -43,7 +28,7 @@ export const schema = S.Collections({
       updated_at: S.Date({}),
       date: S.Date({}),
       passenger: S.Boolean({}),
-      likelihood: S.Number({}),
+      likelihood: S.Number({}), // TODO: remove this
       active: S.Boolean({}),
       location_id: S.Id(),
       label: S.Optional(S.String()),
@@ -51,22 +36,23 @@ export const schema = S.Collections({
       end: S.String(),
 
       // S.RelationById("locations", "location_id"),
-
-      // CREATE TABLE events (
-      //     id serial4 NOT NULL,
-      //     "uuid" uuid DEFAULT gen_random_uuid() NOT NULL,
-      //     created_at timestamptz DEFAULT now() NOT NULL,
-      //     updated_at timestamptz DEFAULT now() NOT NULL,
-      //     "date" DATE NOT NULL,
-      //     passenger bool NOT NULL,
-      //     likelihood int4 NOT NULL,
-      //     active bool DEFAULT TRUE NOT NULL,
-      //     location_id int4 NOT NULL,
-      //     "label" text NULL,
-      //     "start" TIME NOT NULL,
-      //     "end" TIME NOT NULL,
-      //     constraint events_pkey primary key (id)
-      // );
     }),
+
+    permissions: {
+      anonymous: {
+        read: {
+          filter: [true],
+        },
+        insert: {
+          filter: [true],
+        },
+        update: {
+          filter: [true],
+        },
+        delete: {
+          filter: [true],
+        },
+      },
+    },
   },
 });
